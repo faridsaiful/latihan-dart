b9e4d0851febe1fe227209e5bf11fd49391c6a6e
faridsaiful <miztallizer@gmail.com>
2024-06-20 14:43:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApps());
+}
+
+class MyApps extends StatelessWidget {
+  const MyApps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
